8c7fcbea232e8a25ea523f027561b3c3c52b12ac
ChrisNT23 <105956304+ChrisNT23@users.noreply.github.com>
2024-05-07 22:04:29 -0500

    SOAP web services taller

---
+++ Cliente/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cliente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServicioMio.Service1Client oClient =  new ServicioMio.Service1Client();
+            string res = oClient.GetData(20);
+            MessageBox.Show(res);
+
+            ServicioMio.CompositeType oData = new ServicioMio.CompositeType();
+            oData.BoolValue = true;
+            var res2 = oClient.GetDataUsingDataContract(oData);
+            MessageBox.Show(res2.StringValue);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ServicioMio.Service1Client oClient2 = new ServicioMio.Service1Client();
+            string res = oClient2.GetData2(90);
+            MessageBox.Show(res);
+
+            ServicioMio.CompositeType oData = new ServicioMio.CompositeType();
+            oData.BoolValue = true;
+            var res2 = oClient2.GetDataUsingDataContract(oData);
+            MessageBox.Show(res2.StringValue);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia del cliente del servicio
+            ServicioMio.Service1Client oClient = new ServicioMio.Service1Client();
+
+            // Llamar al método GetWeatherInfo para obtener la información meteorológica
+            string location = "New York"; // Ubicación para la cual deseas obtener la información meteorológica
+            string weatherInfo = oClient.GetWeatherInfo(location);
+
+            // Mostrar la información meteorológica en un cuadro de mensaje
+            MessageBox.Show(weatherInfo);
+        }
+
+    }
+}
---
+++ SoapWebServicesC#/Service1.svc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SoapWebServicesC_
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("Los Grados en Farenheit son: {0}", (value * 9/5)+32);
+            
+        }
+
+        public string GetData2(int value2)
+        {
+            return string.Format("Los grados en Centígrados son: {0}", (value2 - 32)* 5/9);
+        }
+
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Gracias";
+            }
+            return composite;
+        }
+
+        public string GetWeatherInfo(string location)
+        {
+            // Aquí puedes implementar la lógica para obtener información meteorológica
+            // Puedes simular la respuesta por ahora
+            WeatherInfo weather = new WeatherInfo
+            {
+                Temperature = "25°C",
+                Humidity = "60%",
+                WindSpeed = "10 km/h"
+            };
+
+            // Devuelve la información meteorológica serializada como JSON
+            return SerializeToString(weather);
+        }
+
+        private string SerializeToString(WeatherInfo weather)
+        {
+            // Formatea la información meteorológica como una cadena de texto
+            return string.Format("Temperatura: {0}\nHumedad: {1}\nVelocidad del Viento: {2}",
+                                 weather.Temperature, weather.Humidity, weather.WindSpeed);
+        }
+
+
+    }
+}
